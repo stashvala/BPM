a9e069ef407125b73bbc4dfc87ffac6d8975df92
stas <user.name>
2017-12-16 13:56:21 +0100

    Formatted code

--- bpm.py
+++ bpm.py
@@ -1,5 +1,5 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 
 class BPM:
@@ -13,30 +13,33 @@ class BPM:
     def predict(self, train_csv_path):
         pass
 
+    def structure_data(self):
+        cnt = 0
+        time_arr = [x.split(" ")[2] for x in self.data[' Čas prejema']]
+        data_array = np.vstack((self.data[' Ime koraka'], self.data['Številka instance'], self.data[' Številka naloge'], time_arr)).T
+        data_array = data_array[data_array[:, 1].argsort()]
+
+        previous_instance = data_array[0, 1]
+        instance_dict = dict()
+        instance_array = [data_array[0]]
+        for i in data_array:
+            # for i in data[' Ime koraka']:
+            if i[1] != previous_instance:
+                npArr = np.array(instance_array)
+                # sort by time, save to dict
+                instance_dict[previous_instance] = npArr[npArr[:, 3].argsort()]
+                instance_array = []
+                previous_instance = i[1]
+
+            instance_array.append(i)
+
+        # print out instances
+        for i in instance_dict:
+            for j in instance_dict[i]:
+                print(j)
+            print('---------------------')
+
 if __name__ == '__main__':
     bpm = BPM('data/train.csv')
-    data = bpm.data
-    cnt = 0
-    time_arr = [x.split(" ")[2] for x in data[' Čas prejema']]
-    data_array = np.vstack((data[' Ime koraka'],data['Številka instance'], data[' Številka naloge'], time_arr)).T
-    data_array = data_array[data_array[:,1].argsort()]
-    
-    previous_instance = data_array[0,1]
-    instance_dict = dict()
-    instance_array = [data_array[0]]
-    for i in data_array:
-    #for i in data[' Ime koraka']:
-    	if i[1] != previous_instance:
-    		npArr = np.array(instance_array)
-    		#sort by time, save to dict
-    		instance_dict[previous_instance] = npArr[npArr[:,3].argsort()]
-    		instance_array = []
-    		previous_instance = i[1]
+    bpm.structure_data()
 
-    	instance_array.append(i)
-
-   	#print out instances
-    for i in instance_dict:
-    	for j in instance_dict[i]:
-    		print(j)
-    	print('---------------------')
