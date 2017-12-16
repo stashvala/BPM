bb3223056393876891c0da34d957c27012e696a0
stas <user.name>
2017-12-16 21:21:34 +0100

    Added visualization of transition matrix

--- bpm.py
+++ bpm.py
@@ -4,10 +4,13 @@ import numpy as np
 import pandas as pd
 import time
 from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
 
 
 class BPM:
     def __init__(self, traincsv_path):
+        self.transitions = None
+        self.transitions_counts = None
         self._parse_data(traincsv_path)
         self._instance_dict = self._structure_data()
         self._hmm_initial_params()
@@ -93,6 +96,7 @@ class BPM:
 
         transition_sums = {key: reduce(lambda x, y: transitions[key][y] + x, transitions[key], 0) for key in
                            transitions}
+        self.transitions_counts = transitions
         transitions = {
         key: {k: transitions[key][k] / transition_sums[key] if transitions[key][k] != 0 else 0 for k in transitions} for
         key in transitions}
@@ -103,6 +107,7 @@ class BPM:
         for i in transitions:
             verbose and print(str(i) + " " + str(reduce(lambda x, y: x + transitions[i][y], transitions[i], 0)) + " " + str(
                 transitions[i]))
+        self.transitions = transitions
 
         return states, start_probability, transitions
 
@@ -138,6 +143,53 @@ class BPM:
 
         return {key: val["duration"]/val["cnt"] for key, val in avg_times.items()}
 
+    def visualize_confusion_mat(self):
+        conf_mat = []
+        norm_conf = []
+        for i in self.transitions_counts.keys():
+            tmp = []
+            tmp_norm = []
+            assert(self.transitions_counts.keys() == self.transitions_counts[i].keys())
+            for j in self.transitions_counts[i]:
+                tmp.append(self.transitions_counts[i][j])
+                tmp_norm.append(self.transitions[i][j])
+            conf_mat.append(tmp)
+            norm_conf.append(tmp_norm)
+
+        conf_mat = np.array(conf_mat)
+
+        print(conf_mat)
+        # norm_conf = []
+        # for i in conf_mat:
+        #     a = 0
+        #     tmp_arr = []
+        #     a = sum(i, 0)
+        #     for j in i:
+        #         tmp_arr.append(float(j) / float(a))
+        #     norm_conf.append(tmp_arr)
+
+        fig = plt.figure(figsize=(13, 25))
+        plt.clf()
+        ax = fig.add_subplot(111)
+        ax.set_aspect(1)
+        res = ax.imshow(np.array(norm_conf), cmap=plt.cm.jet,
+                        interpolation='nearest')
+
+        width, height = conf_mat.shape
+
+        for x in range(width):
+            for y in range(height):
+                ax.annotate(str(conf_mat[x][y]), xy=(y, x),
+                            horizontalalignment='center',
+                            verticalalignment='center')
+
+        cb = fig.colorbar(res)
+        class_labels = self.transitions.keys()
+        plt.xticks(range(width), class_labels, rotation=45)
+        plt.yticks(range(height), class_labels)
+
+        plt.show()
+
     def predict_with_avg(self, avg_times):
         train_data = np.vstack((self.data[' Ime koraka'], self.data['Številka instance'], self.data[' Številka naloge'], self.data[' Čas začetka'], self.data[' Čas zaključka'])).T
 
@@ -164,5 +216,6 @@ class BPM:
 
 if __name__ == '__main__':
     bpm = BPM('data/train.csv')
-    average_times = bpm.avg(True)
-    bpm.predict_with_avg(average_times)
+    #average_times = bpm.avg(True)
+    #bpm.predict_with_avg(average_times)
+    bpm.visualize_confusion_mat()
